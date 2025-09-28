3313f4f1f3503dab82fd024577ef6fb91d30a7f2
dandrades <andradesdiego@gmail.com>
2025-09-28 21:09:52 +0200

    fix: production rewrite for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,15 +7,15 @@ export default defineConfig({
   server: {
     proxy: {
       // Proxy para la API de iTunes
-      "/rss": {
-        target: "https://itunes.apple.com/us",
+      '/rss': {
+        target: 'https://itunes.apple.com/us',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/rss/, "/rss"),
+        rewrite: (path) => path.replace(/^\/rss/, '/rss'),
       },
-      "/lookup": {
-        target: "https://itunes.apple.com",
+      '/lookup': {
+        target: 'https://itunes.apple.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/lookup/, "/lookup"),
+        rewrite: (path) => path.replace(/^\/lookup/, '/lookup'),
       },
     },
   },
